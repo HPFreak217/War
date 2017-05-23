68648bcca5f7cc675a05ae848cead1005465898b
HPFreak217 <carlos.hurtado19@yahoo.com>
2017-05-23 12:22:49 -0700

    Initial Commit

---
+++ War/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  War
+//
+//  Created by Carlos Hurtado on 5/23/17.
+//  Copyright © 2017 Carlos Hurtado. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
